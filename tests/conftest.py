8feda079318bd062863edf902e8fa8e791d3b441
JavaOPgotanda3a <85466949+JavaOPgotanda3a@users.noreply.github.com>
2026-06-11 08:11:12 +0000

    SyncASGIClientを追加し、テストクライアントを更新

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,35 @@
+import asyncio
 from copy import deepcopy
 
+import httpx
 import pytest
-from fastapi.testclient import TestClient
 
 from src import app as app_module
 
 
 BASE_ACTIVITIES = deepcopy(app_module.activities)
+
+
+class SyncASGIClient:
+    def __init__(self, app):
+        self._app = app
+
+    def request(self, method, url, **kwargs):
+        async def make_request():
+            transport = httpx.ASGITransport(app=self._app)
+            async with httpx.AsyncClient(transport=transport, base_url="http://testserver") as client:
+                return await client.request(method, url, **kwargs)
+
+        return asyncio.run(make_request())
+
+    def get(self, url, **kwargs):
+        return self.request("GET", url, **kwargs)
+
+    def post(self, url, **kwargs):
+        return self.request("POST", url, **kwargs)
+
+    def delete(self, url, **kwargs):
+        return self.request("DELETE", url, **kwargs)
 
 
 @pytest.fixture(autouse=True)
@@ -20,4 +43,4 @@ def reset_activities():
 
 @pytest.fixture()
 def client():
-    return TestClient(app_module.app)
+    return SyncASGIClient(app_module.app)
